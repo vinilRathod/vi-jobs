7c69030d46211eb7d446d5603a1b497e0da6df66
Vinil Rathod <viniljain703@gmail.com>
2021-11-05 18:26:04 +0530

    added interview prep section

--- src/Pages/Employee/Interview.js
+++ src/Pages/Employee/Interview.js
@@ -28,35 +28,35 @@ const Interview = () =>{
                 <div class="card-body">
                     <h4 class="card-title">DSA Cracker</h4>
                     <p class="card-text">Following is the link of 450 DSA Questions made by Love Babbar</p>
-                    <a href="https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbDI2dWhieGtlSTdmLTctcEowWmp5VnRCejR1QXxBQ3Jtc0tuajJxcU5CNFgwSlFsZm5LZHljeGd5VzhJQU9Ua0JVeFlkNlBmSWtHTlM3UXRRUUlwUGpZbFdLa0FXQmt6QUxmeG80VlVva280ZUhqT2NsdTUzQkNHWWJ0VHBwSHIzdGpSTmRuR2RTV2RGZ2VBVmZtVQ&q=https%3A%2F%2Fdrive.google.com%2Ffile%2Fd%2F1FMdN_OCfOI0iAeDlqswCiC2DZzD4nPsb%2Fview%3Fusp%3Dsharing" class="card-link">DSA Sheet</a>
+                    <a href="https://drive.google.com/file/d/1FMdN_OCfOI0iAeDlqswCiC2DZzD4nPsb/view" class="card-link">DSA Sheet</a>
                 </div>
             </div>
             <div class="card bg-light">
                 <div class="card-body">
                     <h4 class="card-title">Operating System Notes</h4>
                     <p class="card-text">Following is the link of OS Notes made by Apna College(YT)</p>
-                    <a href="https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbUxfUUh0RE1VWnZXTmlrZjdEMkR2c1RyaTI2d3xBQ3Jtc0trZ2JPY2w4MWgxWVhEVkRJbXl3NU9VdkFhYVBpTnhjUHRzNW5SMlNxZHFsaTlsbVl3RGJBRGtWZk85QmN3eFRhOHlTblIwUHJhZkFSZDRpR2h0cXA2YkJmb1NZOFB5SlNRY2x3Q2hkcm9jUUg3RDFaTQ&q=https%3A%2F%2Fdrive.google.com%2Ffile%2Fd%2F1B_kmekdghu-sO8-eXZ1kc4Xx_YtB8BPM%2Fview%3Fusp%3Dsharing" class="card-link">OS Notes</a>
+                    <a href="https://drive.google.com/file/d/1B_kmekdghu-sO8-eXZ1kc4Xx_YtB8BPM/view" class="card-link">OS Notes</a>
                 </div>
             </div>
             <div class="card bg-light">
                 <div class="card-body">
                     <h4 class="card-title">Computer Network Notes</h4>
                     <p class="card-text">Following is the link of CN Notes made by Apna College(YT)</p>
-                    <a href="https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbnRMYWVJQUFCOThaN3Q5cjZwOWRWSFJZOVdPQXxBQ3Jtc0tsamhPUjNZMG1lSmc3enZLUXY4LWJMX3NGOGwwWHJXZ2U4TTB6alhsbGdYbE0xUlVSX0VyVGN5NTlJeUpnTS1Ic3RXZnZkSmgwSnhOSTZJMTFVSXRhRy1ySkNfM2diMUFKZlVjTzNnY1VyLXhoelBfZw&q=https%3A%2F%2Fdrive.google.com%2Ffile%2Fd%2F1EraWa_yVfFHJqOMG2nG91fbg2g1FHm30%2Fview%3Fusp%3Dsharing" class="card-link">CN Notes</a>
+                    <a href="https://drive.google.com/file/d/1EraWa_yVfFHJqOMG2nG91fbg2g1FHm30/view" class="card-link">CN Notes</a>
                 </div>
             </div>
             <div class="card bg-light">
                 <div class="card-body">
                     <h4 class="card-title">Database and Management System Notes</h4>
                     <p class="card-text">Following is the link of DBMS Notes made by Apna College(YT)</p>
-                    <a href="https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbFRiZ09oeTRLX25nd2dGQTIyZHVFRndjNG91UXxBQ3Jtc0tsUmVranNxYUZobG11UTdSTDdWRkc4QTFpbXhmZkRQUGJvazE2Q2hzaFFaTnA2VkE1SDcwcXFmb2lQSFdKRV82NndSX1JfcEhlVGQxZkRGOWVfd1Zqb3V3a2lfaG5NRkdJOWRKOWg2LXM0WlZCSVhVYw&q=https%3A%2F%2Fdocs.google.com%2Fdocument%2Fd%2F1KV-9I5D6iddJbAoDzPEBVR6izf-j_7PXq3JQSC7KC2Q%2Fedit%3Fusp%3Dsharing" class="card-link">DBMS Notes</a>
+                    <a href="https://docs.google.com/document/d/1KV-9I5D6iddJbAoDzPEBVR6izf-j_7PXq3JQSC7KC2Q/view" class="card-link">DBMS Notes</a>
                 </div>
             </div>
             <div class="card bg-light">
                 <div class="card-body">
                     <h4 class="card-title">Object Oriented Programming System Notes</h4>
                     <p class="card-text">Following is the link of OOPS Notes made by Apna College(YT)</p>
-                    <a href="https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbDZTOEN4aWcwcU9hVW1rYk04Q1FjNUgzYVdTZ3xBQ3Jtc0tud1RJaFF1cXQwZjNPcWkwUm54blVCLUgyUTRDQjU1ZVpJemVkel9ZVktlR0NhR1N0czlYMHZhQkxvOHVPZGs4SW5VN1pvYW5qRXBvQjRpSW14LWdzcDloaEh4U0JCcUU2bjNrNjRpR1lPVXBZNEo4WQ&q=https%3A%2F%2Fdrive.google.com%2Ffile%2Fd%2F1CljO4lsVcxLXj59X0OMBB5WNhG0fzVOw%2Fview%3Fusp%3Dsharing" class="card-link">OOPS Notes</a>
+                    <a href="https://drive.google.com/file/d/1CljO4lsVcxLXj59X0OMBB5WNhG0fzVOw/view" class="card-link">OOPS Notes</a>
                 </div>
             </div>
             <nav class="navbar navbar-expand-sm bg-dark navbar-dark justify-content-center">
